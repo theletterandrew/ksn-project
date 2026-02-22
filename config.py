49e278be93cba5ea409d968e1c432d3a26e8edbf
Andrew Phillips <andrew.austen.phillips@gmail.com>
2026-02-22 13:08:36 -0600

    Update config.py
    Changes MIN_DRAINAGE_AREA_CELLS to 500000 instead of 100000 because we were getting way too many watersheds.

--- config.py
+++ config.py
@@ -90,7 +90,7 @@ WBT_BREACH_DIST = 100 if TEST_RUN else 1000
 # Watershed minimum drainage area threshold
 # Test dataset: (~0.4 km^2 at 2m resolution)
 # Full dataset: (~40 km² at 2m resolution)
-MIN_DRAINAGE_AREA_CELLS = 100000 if TEST_RUN else 10000000
+MIN_DRAINAGE_AREA_CELLS = 500000 if TEST_RUN else 10000000
 
 # Pour points are snapped to the highest flow accumulation cell within
 # this distance to ensure they land exactly on the stream
