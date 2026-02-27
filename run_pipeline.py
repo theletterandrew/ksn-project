c66f4fffe38b723012be532f99b150cc343d3be5
Andrew Phillips <andrew.austen.phillips@gmail.com>
2026-02-27 09:00:48 -0600

    Update run_pipeline.py
    Uncomments the scripts at the beginning to test the pipeline from the start with the new test area.

--- run_pipeline.py
+++ run_pipeline.py
@@ -30,13 +30,13 @@ def sanitize_env():
 SCRIPTS_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "scripts")
 
 SCRIPTS_TO_RUN = [
-    # "batchdownload.py",
-    # "laz_to_las.py",
-    # "inspect_las.py",
-    # "delete_empty_files.py",
-    # "las_to_dem.py",
-    # "mosaic_dem.py",
-    # "wbt_hydrology.py",
+    "batchdownload.py",
+    "laz_to_las.py",
+    "inspect_las.py",
+    "delete_empty_files.py",
+    "las_to_dem.py",
+    "mosaic_dem.py",
+    "wbt_hydrology.py",
     "stream_extraction_wbt.py",
     "delineate_watersheds.py",
     "clip_watersheds.py",
