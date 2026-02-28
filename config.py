05ebaddd605b201e8c2a67a98fe990ba9997dd99
Andrew Phillips <andrew.austen.phillips@gmail.com>
2026-02-27 22:02:13 -0600

    Update config.py
    Adds min_outlet_separation

--- config.py
+++ config.py
@@ -83,6 +83,9 @@ MIN_DRAINAGE_AREA_CELLS = 900000 if TEST_RUN else 10000000    # cells (~4 km² a
 # Edge cells drain off-raster in D8, accumulating spurious flow.
 BORDER_CELLS = 3
 
+# Minimum distance separating the outlet points (in meters)
+MIN_OUTLET_SEPARATION = 1000
+
 # --- DELETE EMPTY FILES PARAMETERS ---
 MIN_TILE_SIZE_KB = 1
 
