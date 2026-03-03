ec714966b2862f67d0608c6ce4a99679b9740fc6
Andrew Phillips <andrew.austen.phillips@gmail.com>
2026-03-03 08:16:00 -0600

    Update test_watershed_dtype.py
    debugging

--- scripts/test_watershed_dtype.py
+++ scripts/test_watershed_dtype.py
@@ -8,3 +8,15 @@ with rasterio.open(fdr) as s:
 
 with rasterio.open(mask) as s:
     print("MASK :", s.width, s.height, s.transform, s.crs, s.nodata, s.dtypes)
+
+
+with rasterio.open(r"E:\ksn-project\data\watersheds\watershed_1_fdr.tif") as src:
+    arr = src.read(1)
+    nd  = src.nodata
+
+valid = arr[arr != nd]
+print("Unique FDR values:", np.unique(valid))
+
+import subprocess
+result = subprocess.run([r"E:\ksn-project\bin\WBT\whitebox_tools.exe", "--version"], capture_output=True, text=True)
+print(result.stdout)
