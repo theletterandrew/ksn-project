372c25ce44fcd62f0f005406fd0f138f01fd2444
Andrew Phillips <andrew.austen.phillips@gmail.com>
2026-03-03 08:07:33 -0600

    debugging

--- scripts/test_watershed_dtype.py
+++ scripts/test_watershed_dtype.py
@@ -1,3 +1,10 @@
 import rasterio
-with rasterio.open(r"E:\ksn-project\data\watersheds\watershed_1_fdr.tif") as src:
-    print(src.nodata, src.dtypes, src.crs)
+
+fdr  = r"E:\ksn-project\data\watersheds\watershed_1_fdr.tif"
+mask = r"E:\ksn-project\data\scratch\watersheds\_ws_mask_1.tif"
+
+with rasterio.open(fdr) as s:
+    print("FDR  :", s.width, s.height, s.transform, s.crs, s.nodata, s.dtypes)
+
+with rasterio.open(mask) as s:
+    print("MASK :", s.width, s.height, s.transform, s.crs, s.nodata, s.dtypes)
